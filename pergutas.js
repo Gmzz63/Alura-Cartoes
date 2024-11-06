51accfa76b0eba5bfe1aee0432cf601c532e2c8c
Gmzz63 <00001124068703sp@al.educacao.sp.gov.br>
2024-11-06 15:18:02 -0300

    Add files via upload

---
+++ pergutas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Progamação',
+    'O que é Python ?',
+    'O JavaScript é uma linguagem de progamção'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da frnça ?',
+    'a capital da frança é Paris'
+)
+
+criaCartao(
+    'Progamação',
+    'O que é uma função ?',
+    'É um bloco de codigo que exucuta alguma função'
+)
+
+criaCartao(
+    'Lingua Inglesa',
+    'Como se diz oi em inglẽs',
+    'Oi em inglẽs é hi'
+)
